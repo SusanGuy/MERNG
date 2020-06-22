2ed0a830ae227ed6b6d9055e4646c4d67494678d
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-06-22 16:14:01 -0500

    User and Post Model done

--- index.js
+++ index.js
@@ -2,16 +2,31 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 const { MONGODB } = require("./config");
+const Post = require("./models/Post");
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+
   type Query {
-    sayHi: String!
+    getPosts: [Post!]!
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => "Hello World",
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
 
@@ -20,17 +35,15 @@ const server = new ApolloServer({
   resolvers,
 });
 
-mongoose.connect(
-  MONGODB,
-  {
+mongoose
+  .connect(MONGODB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  },
-  () => {
+  })
+  .then(() => {
     console.log("MongoDB server connected");
-  }
-);
-
-server.listen({ port: 5000 }).then(({ url }) => {
-  console.log("Server running at " + url);
-});
+    return server.listen({ port: 5000 });
+  })
+  .then(({ url }) => {
+    console.log("Server running at " + url);
+  });
