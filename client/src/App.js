eb92d8e5a6f795b1459106e907a03e545a76a354
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-06-22 21:50:43 -0500

    Menu Bar Component added and Routing done

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+import MenuBar from "../src/components/MenuBar";
+import Home from "../src/containers/Home";
+import Login from "../src/containers/Login";
+import Register from "../src/containers/Register";
+import { Container } from "semantic-ui-react";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-    </Router>
+    <Container>
+      <Router>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Router>
+    </Container>
   );
-}
+};
 
 export default App;
