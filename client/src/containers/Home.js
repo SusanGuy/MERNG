e807c62c8ed964aff62587f6e36afdf9095ec9cd
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-06-22 22:18:43 -0500

    Home container done and fetching all posts query done

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,11 +1,63 @@
 import React from "react";
-
+import { useQuery } from "@apollo/react-hooks";
+import { Grid, GridColumn } from "semantic-ui-react";
+import PostCard from "../components/PostCard";
+import gql from "graphql-tag";
 const Home = () => {
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+  let posts = [];
+  if (data) {
+    posts = data.getPosts;
+  }
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading Posts....</h1>
+        ) : (
+          posts &&
+          posts.map((post) => {
+            return (
+              <GridColumn
+                key={post.id}
+                style={{
+                  marginBottom: "20px",
+                }}
+              >
+                <PostCard post={post} />
+              </GridColumn>
+            );
+          })
+        )}
+      </Grid.Row>
+    </Grid>
   );
 };
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default Home;
